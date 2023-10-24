e6964420d2ff48bb393b10074f176b24ea67cdb6
Greblips <Evgenspilbegrg@gmail.com>
2023-10-24 17:23:19 +0300

    Продолжаю раскидывать на компоненты

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import AudioPlayer from "./components/AudioPlayer";
+import Footer from "./components/Footer";
+import Navmenu from "./components/Navmenu";
+import Sidebar from "./components/Sidebar";
+import Tracklist from "./components/Tracklist";
+
 
 const App = () => {
-  return <div className="text">Здесь скоро будет Skypro.Music</div>;
+  return <div className="wrapper">
+    <div className="container">
+      <main className="main">
+        <Navmenu />
+        < Tracklist />
+        <Sidebar />
+      </main >
+      <AudioPlayer />
+      <Footer />
+    </div >
+  </div >
 };
 
 export default App;
