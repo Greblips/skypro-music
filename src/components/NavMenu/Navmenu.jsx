514d6e85bdde38e08f2bc6447e7fabf2a94fd0f7
Greblips <Evgenspilbegrg@gmail.com>
2023-10-25 16:50:54 +0300

    Добавил бургер меню

--- src/components/NavMenu/Navmenu.jsx
+++ src/components/NavMenu/Navmenu.jsx
@@ -1,24 +1,29 @@
 
 import "./NavMenu.css";
 import NavMenuItems from '../NavMenuItems/NavMenuItems';
+import { useState } from "react";
 const Navmenu = () => {
+    const [burrgerVisible, setBurgherVisible] = useState(false);
+    const toggleBurgherVisibilily = () => setBurgherVisible(!burrgerVisible)
     return (
         <nav className="main__nav nav">
             <div className="nav__logo logo">
                 <img className="logo__image" src="img/logo.png" alt="logo" />
             </div>
-            <div className="nav__burger burger">
+            <div className="nav__burger burger" type="button" onClick={toggleBurgherVisibilily} >
                 <span className="burger__line"></span>
                 <span className="burger__line"></span>
                 <span className="burger__line"></span>
             </div>
-            <div className="nav__menu menu">
-                <ul className="menu__list">
-                    <NavMenuItems item={{ link: "#", text: "Главное" }} />
-                    <NavMenuItems item={{ link: "#", text: "Мой плейлист" }} />
-                    <NavMenuItems item={{ link: "../signin.html", text: "Войти" }} />
-                </ul>
-            </div>
+            {burrgerVisible && (
+                <div className="nav__menu menu">
+                    <ul className="menu__list">
+                        <NavMenuItems item={{ link: "#", text: "Главное" }} />
+                        <NavMenuItems item={{ link: "#", text: "Мой плейлист" }} />
+                        <NavMenuItems item={{ link: "../signin.html", text: "Войти" }} />
+                    </ul>
+                </div>
+            )}
         </nav>
     );
 };
