24a479ea1a8e17360aa13b54e421a0fde605adf5
Greblips <Evgenspilbegrg@gmail.com>
2023-10-30 17:17:54 +0300

    Сделал NavMenu

--- src/components/NavMenu/Navmenu.jsx
+++ src/components/NavMenu/Navmenu.jsx
@@ -1,30 +1,35 @@
 
-import "./NavMenu.css";
 import NavMenuItems from '../NavMenuItems/NavMenuItems';
 import { useState } from "react";
+import * as S from './NavMenu.styles'
+
+
 const Navmenu = () => {
     const [burrgerVisible, setBurgherVisible] = useState(false);
-    const toggleBurgherVisibilily = () => setBurgherVisible(!burrgerVisible)
+
     return (
-        <nav className="main__nav nav">
-            <div className="nav__logo logo">
-                <img className="logo__image" src="img/logo.png" alt="logo" />
-            </div>
-            <div className="nav__burger burger" type="button" onClick={toggleBurgherVisibilily} >
-                <span className="burger__line"></span>
-                <span className="burger__line"></span>
-                <span className="burger__line"></span>
-            </div>
-            {burrgerVisible && (
-                <div className="nav__menu menu">
-                    <ul className="menu__list">
-                        <NavMenuItems item={{ link: "#", text: "Главное" }} />
-                        <NavMenuItems item={{ link: "#", text: "Мой плейлист" }} />
-                        <NavMenuItems item={{ link: "../signin.html", text: "Войти" }} />
-                    </ul>
-                </div>
-            )}
-        </nav>
+        <S.MainNav >
+            <S.NavLogo>
+                <S.Logoimage src="img/logo.png" alt="logo" />
+            </S.NavLogo>
+            <S.NavBurger onClick={() => setBurgherVisible(!burrgerVisible)} >
+                <S.BurgerLine />
+                <S.BurgerLine />
+                <S.BurgerLine />
+
+            </S.NavBurger>
+            {
+                burrgerVisible && (
+                    <S.NavMenu>
+                        <S.MenuList>
+                            <NavMenuItems item={{ link: "#", text: "Главное" }} />
+                            <NavMenuItems item={{ link: "#", text: "Мой плейлист" }} />
+                            <NavMenuItems item={{ link: "../signin.html", text: "Войти" }} />
+                        </S.MenuList>
+                    </S.NavMenu>
+                )
+            }
+        </S.MainNav >
     );
 };
 
