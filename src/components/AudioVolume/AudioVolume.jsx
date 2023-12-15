9377c597f7abaf2f495dc7a60a35c6a0e28fbea0
Greblips <Evgenspilbegrg@gmail.com>
2023-12-15 11:52:21 +0300

    complete task

--- src/components/AudioVolume/AudioVolume.jsx
+++ src/components/AudioVolume/AudioVolume.jsx
@@ -1,46 +1,40 @@
+/* eslint-disable no-param-reassign */
 import { useState, useEffect } from "react";
 import * as S from "./AudioVolume.style";
 
-export const AudioVolume = ({ audioRef }) => {
+export function AudioVolume({ audioRef }) {
+  const [volume, setVolume] = useState(40);
+  const [isVolume, setIsVolume] = useState(false);
+  useEffect(() => {
+    if (audioRef) {
+      audioRef.current.volume = volume / 100;
+    }
+  }, [volume, audioRef]);
 
-    const [volume, setVolume] = useState(40);
-    const [isVolume, setIsVolume] = useState(false);
+  const toggleTrackMute = () => {
+    audioRef.current.muted = !isVolume;
+    setIsVolume(!isVolume);
+  };
 
-    useEffect(() => {
-
-        setIsVolume(false);
-        if (audioRef) {
-            audioRef.current.volume = volume / 100;
-
-        }
-
-    }, [volume, audioRef,]);
-
-
-    const toggleTrackMute = () => {
-        audioRef.current.muted = !isVolume;
-        setIsVolume(!isVolume);
-    };
-
-    return (
-        <S.barVolumeBlock>
-            <S.volumeContent>
-                <S.volumeImage>
-                    <S.volumeSvg alt="volume" onClick={() => toggleTrackMute()} >
-                        <use xlinkHref={isVolume ? "img/icon/sprite.svg#icon-volume-off" : "img/icon/sprite.svg#icon-volume"} />
-                    </S.volumeSvg>
-                </S.volumeImage>
-                <S.volumeProgress>
-                    <S.volumeProgressLine
-                        $style="input"
-                        type="range"
-                        min={0}
-                        max={100}
-                        value={volume}
-                        onChange={(e) => setVolume(e.target.value)}
-                    />
-                </S.volumeProgress>
-            </S.volumeContent>
-        </S.barVolumeBlock >
-    );
+  return (
+    <S.barVolumeBlock>
+      <S.volumeContent>
+        <S.volumeImage>
+          <S.volumeSvg alt="volume" onClick={() => toggleTrackMute()}>
+            <use xlinkHref={isVolume ? "img/icon/sprite.svg#icon-volume-off" : "img/icon/sprite.svg#icon-volume"} />
+          </S.volumeSvg>
+        </S.volumeImage>
+        <S.volumeProgress>
+          <S.volumeProgressLine
+            $style="input"
+            type="range"
+            min={0}
+            max={100}
+            value={volume}
+            onChange={(e) => setVolume(e.target.value)}
+          />
+        </S.volumeProgress>
+      </S.volumeContent>
+    </S.barVolumeBlock>
+  );
 }
